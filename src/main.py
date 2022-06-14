cf5b29438c8d106779a6137d9cbae828bf700ae4
dudushy <eduardotalarico1337@gmail.com>
2022-06-14 08:40:53 -0300

    Read and filter only numbers, then assign value

--- src/main.py
+++ src/main.py
@@ -9,8 +9,20 @@ path = os.path.dirname(__file__) #? Directory path
 def clearConsole() -> None: #? Clear console
     os.system("cls" if os.name == "nt" else "clear")
 
-def main() -> None:
-    pass
+def main() -> None:  # sourcery skip: move-assign-in-block, use-join
+    clearConsole()
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    raw_input = input("raw_input: ")
+    aux = ""
+    value = 0
+
+    for i in range(len(raw_input)):
+        if (raw_input[i] in numbers):
+            aux += raw_input[i]
+    value = int(aux)
+
+    print(value)
 
 #! Main
 if __name__ == "__main__":
